b5e75f066051ddf7e5695b434d7a1e3929b9ab6f
OAyUliko <54901300+OAyUliko@users.noreply.github.com>
2019-09-17 21:18:15 +0800

    Update RationalMatrix.java

--- generic矩阵/RationalMatrix.java
+++ generic矩阵/RationalMatrix.java
@@ -1,21 +1,26 @@
 package demo;
-
-public class RationalMatrix extends GenericMatrix<String >{
-
-    @Override
-    protected String add(String e1, String e2) {
-        return e1+e2;
-    }
-
+//合理矩阵？？
+public class RationalMatrix extends GenericMatrix<Number>{
+    //Number里没有String这种字符型啊..
 
     @Override
-    protected String multiply(String e1, String e2) {
-        String e3=new String;
-
+    protected Number add(Number e1, Number e2) {
+        String E1 = String.valueOf((Number) e1);//强制将e1number型换成string，保存在E1
+        String E2 = String.valueOf((Number) e2);
+        Number E3=Integer.parseInt(E1)+Integer.parseInt(E2);//强制将E1字符型结果转换成整型
+        return E3;
     }
 
     @Override
-    protected String zero(String e1, String e2) {
+    protected Number multiply(Number e1, Number e2) {
+        String E1 = String.valueOf((Number) e1);//强制将e1number型换成string，保存在E1
+        String E2 = String.valueOf((Number) e2);
+        Number E3=Integer.parseInt(E1)*Integer.parseInt(E2);//强制将E1字符型结果转换成整型,但是感觉不是这么写的...
+        return E3;
+    }
+
+    @Override
+    protected Number zero(Number e1, Number e2) {
         return null;
     }
 }
