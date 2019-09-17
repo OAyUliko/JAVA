d9f3def7a0f7e03df77de1437ede84c1bca722bc
OAyUliko <54901300+OAyUliko@users.noreply.github.com>
2019-09-17 21:17:53 +0800

    Update TestRationalMatrix.java

--- generic矩阵/TestRationalMatrix.java
+++ generic矩阵/TestRationalMatrix.java
@@ -1,8 +1,8 @@
 package demo;
 
-
-
-public abstract class TestRationalMatrix {
-
-
+public class TestRationalMatrix {
+    char[][] Matrix_1= new char[][]{{'A','B'},{0,0},{1,1}};
+    Number[][] Matrix_2=new Number[][]{{0,1,1},{1,1,0}};
+    IntegerMatrix result_=new IntegerMatrix();//结果矩阵
+    GenericMatrix.printResult(Matrix_1, Matrix_2, result.addMatrix(Matrix1, Matrix2), '*');
 }
