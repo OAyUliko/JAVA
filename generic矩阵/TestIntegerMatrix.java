19f19b2e8307572172cd0d20caa20a18022c0a94
OAyUliko <54901300+OAyUliko@users.noreply.github.com>
2019-09-17 21:17:25 +0800

    Update TestIntegerMatrix.java

--- generic矩阵/TestIntegerMatrix.java
+++ generic矩阵/TestIntegerMatrix.java
@@ -1,4 +1,10 @@
 package demo;
 
 public class TestIntegerMatrix {
+    public static void main(String[] args){
+        Integer[][] Matrix1=new Integer[][]{{1,0},{0,0},{1,1}};
+        Integer[][] Matrix2=new Integer[][]{{0,1,1},{1,1,0}};
+        IntegerMatrix result=new IntegerMatrix();//结果矩阵
+        GenericMatrix.printResult(Matrix1, Matrix2, result.addMatrix(Matrix1, Matrix2), '+');
+    }
 }
