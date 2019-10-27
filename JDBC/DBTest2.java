d9abe9f5c9f02a93383be4ba89a8d02e479be7e6
OAyUliko <54901300+OAyUliko@users.noreply.github.com>
2019-10-27 10:54:42 +0800

    Add files via upload

--- JDBC/DBTest2.java
+++ JDBC/DBTest2.java
@@ -2,22 +2,25 @@ package JDBC;
 
 import java.sql.*;
 
-public  class DBTest {
-    public void test() throws SQLException, ClassNotFoundException {
-        String driverName="com.mysql.jdbc.Driver";//驱动名称  mysql-connector-java 5不用cj.
-        String url="jdbc:mysql://localhost:3306/teaching?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8&useSSL=false";
-        String userName="root";
+public class DBTest2 {
+    public void test2() throws ClassNotFoundException, SQLException {
+        String driverName="com.microsoft.sqlserver.jdbc.SQLServerDriver";//SQL Server驱动名称
+        String url="jdbc:sqlserver://192.168.0.104:1433;DatabaseName=teaching";
+        //JDK为1.6以下的jdbc连接的jar使用sqljdbc.jar,以上使用sqljdbc4.jar
+        String userName="sa";
         String password="5650656";
         Class.forName(driverName);//　Java加载数据库驱动的方法是调用Class类的静态方法forName
         Connection connection= DriverManager.getConnection(url,userName,password);
         //DriverManager注册驱动 getConnection是DriverManager里面的方法
         //对数据库进行创建，插入
-        String sql1="create table student2(sno int primary key,sname char(20))";//建表student2，设置sno,sname
-        String sql2="insert into student2 values(201707771,'陆瑶')";//建表student2，设置sno,sname
+        String sql1="CREATE TABLE student2(sno INT PRIMARY KEY ,sname char(20) NOT NULL)";//建表student2，设置sno,sname
+        String sql2="INSERT INTO student2 VALUES(201707771,'陆瑶')";//建表student2，设置sno,sname
         Statement statement=connection.createStatement();//获取Statement对象
         statement.execute(sql1);
         statement.execute(sql2);
+
         System.out.println("处理完毕！");
+
         statement.close();
     }
 }
